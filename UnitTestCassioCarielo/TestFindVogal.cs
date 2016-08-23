23cf4dd98ae1d4463035e2f4e569ab854c145db1
CassioCarielo <cassiocarielo@gmail.com>
2016-08-23 18:14:23 -0300

    Find Vogal

---
+++ UnitTestCassioCarielo/TestFindVogal.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Collections.Generic;
+using TesteCassioCarielo;
+
+namespace UnitTestCassioCarielo
+{
+    [TestClass]
+    public class TestFindVogal
+    {
+        [TestMethod]
+        public void TestVogalValid()
+        {
+            StringStream stream = new StringStream("aAbBABac");
+            FindVogal findVogal = new FindVogal();
+            char resultado;
+            try
+            {
+                resultado = findVogal.firstChar(stream);
+            }
+            catch (System.Exception)
+            {
+                resultado = ' ';
+            }
+            Assert.IsTrue(resultado != ' ');
+        }
+
+        [TestMethod]
+        public void TestVogalInvalid()
+        {
+            StringStream stream = new StringStream("aaaaaaaaa");
+            FindVogal findVogal = new FindVogal();
+            char resultado;
+            try
+            {
+                resultado = findVogal.firstChar(stream);
+            }
+            catch (System.Exception)
+            {
+                resultado = ' ';
+            }
+            Assert.IsTrue(resultado == ' ');
+        }
+    }
+}
